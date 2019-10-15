bb776c4721f2967477f671549746126bdc7bb606
sidwang2018 <sid@iwork.net.tw>
2019-10-15 12:57:58 +0800

    OKOKO3

--- app_bak.js
+++ app_bak.js
@@ -20,18 +20,15 @@ const app = express();
 // [START hello_world]
 // Say hello!
 app.get('/', (req, res) => {
-    res.status(200).send('Hello, world123456by sid!');
+    res.status(200).send('Hello, world by sid!');
 });
 // [END hello_world]
 
-if (module === require.main) {
-    // [START server]
     // Start the server
-    const server = app.listen(process.env.PORT || 80, () => {
+ const server = app.listen(process.env.PORT || 8080, () => {
         const port = server.address().port;
         console.log(`App listening on port ${port}`);
     });
     // [END server]
-}
 
 module.exports = app;
