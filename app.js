d1c83ee4fd920297c1e81513b4fc24a068f99b39
sidwang2018 <sid@iwork.net.tw>
2019-10-17 18:02:57 +0800

    okokok

--- app.js
+++ app.js
@@ -2,24 +2,20 @@ const express = require('express')
   , fs = require('fs');
 const imgFolder = './ftpdata/';
 const app = express();
-console.log('after');
 var favicon = require('serve-favicon');
 app.use(favicon('./ftpdata/favicon.ico'))
 
 function Getfname(camid) {
   return new Promise((resolve, reject) => {
-    console.log(imgFolder + camid + '/');
     var fulname;
     fs.readdir(imgFolder + camid + '/', (err, files) => {
-      console.log(typeof files);
       if ((typeof files != "undefined") && (files.length > 0)) { fulname = imgFolder + camid + '/' + files[files.length - 1]; }
       else { fulname = imgFolder + '/nopic.jpg'; }
-  console.log("I am " + fulname);
+      console.log("I am " + fulname);
       resolve({ camid: camid, fname: fulname });
     });
   });
 }
-
 function Showimg(fname, res) {
   fs.readFile(fname, function (err, imgdata) {
     if (err) {
@@ -59,9 +55,15 @@ const port = 21;
 var sdir = 'mypath';
 const ftpServer = new FtpSvr('ftp://' + hostname + ':' + port,
   { anonymous: false, greeting: ["您好", "歡迎"], pasv_min:3000,pasv_max:4000,pasv_url:"0.0.0.0" });
+ftpServer.pasv_url = "0.0.0.0";
+ftpServer.pasv_min = 3000;
+ftpServer.pasv_url = 4000;
+
+console.log(ftpServer.pasv_min);
 
 ftpServer.on('login', ({ connection, username, password }, resolve, reject) => {
   console.log('username: ' + username);
+  console.log("pasv_min:"+ftpServer.pasv_min);
   //  console.log('resolve: ' + resolve);
   // console.log('reject: ' + reject);
   if (password == "4kZu2018") {
