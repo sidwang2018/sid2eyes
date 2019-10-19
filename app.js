ec33219c7ace35f9c5229c9dfaccd3ee3bbafd8d
sidwang2018 <sid@iwork.net.tw>
2019-10-19 22:04:01 +0800

    okokok

--- app.js
+++ app.js
@@ -43,50 +43,9 @@ app.get('/', (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
+  console.log(process.env.PORT);
   console.log(`App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
 });
 // [END gae_node_request_example]
-
-
-const FtpSvr = require('ftp-srv');
-const hostname = '0.0.0.0';
-const port = 21;
-var sdir = 'mypath';
-
-
-const ftpServer = new FtpSvr({url:'ftp://' + hostname + ':' + port,
-   anonymous: false, greeting: ["您好", "歡迎"], pasv_min:3000,pasv_max:4000,pasv_url:"0.0.0.0" });
-/* ftpServer.pasv_url = "0.0.0.0";
-ftpServer.pasv_min = 3000;
-ftpServer.pasv_url = 4000;
- */
-console.log(ftpServer.pasv_min);
-
-ftpServer.on('login', ({ connection, username, password }, resolve, reject) => {
-  console.log('username: ' + username);
-  console.log("pasv_min:"+ftpServer.pasv_min);
-  //  console.log('resolve: ' + resolve);
-  // console.log('reject: ' + reject);
-  if (password == "4kZu2018") {
-    sdir = './ftpdata/' + username + '/';
-    console.log(sdir);
-    resolve({ root: sdir });
-  }
-  else
-    reject('pass not Pass');
-
-  console.log(sdir);
-});
-
-ftpServer.on('client-error', (connection, context, error) => {
-  //  console.log('connection: ');
-
-});
-
-ftpServer.listen()
-  .then(() => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-  });
-
 module.exports = app;
